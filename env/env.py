28fa89efa0b2930cf2fd8128819ec7a638cdc707
Karl Hajjar <karl.hajjar@polytechnique.edu>
2018-10-04 19:10:31 +0200

    Initial commit

---
+++ env/env.py
@@ -0,0 +1,28 @@
+import numpy as np
+from rendering import *
+
+class Env(object):
+	"""A class implementing the solitaire environment"""
+
+
+	def __init__(self, arg):
+		'''
+		'''
+		#super(ClassName, self).__init__()
+		self.arg = arg
+
+
+	def reset(self):
+		'''
+		'''
+
+
+	def step(self, action):
+		'''
+		'''
+		return reward, next_state, end
+
+
+	def render(self):
+		'''
+		'''
