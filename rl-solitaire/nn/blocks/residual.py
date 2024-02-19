Added test for conv-residual network and fixed issue with entropy computation.

rl-solitaire/nn/blocks/residual.py
@@ -7,34 +7,49 @@ class ResidualBlock(torch.nn.Module):
     connection.
     """
 
-    def __init__(self, n_layers: int, input_dim: int, hidden_dim: int, activation: torch.nn.Module, layer_type: str,
+    def __init__(self, input_dim: int, hidden_dim: int, activation: torch.nn.Module, layer_type: str, n_layers: int = 2,
                  bias: bool = True, **kwargs):
+        """
+        A residual connection block with a number of intermediate layers. Note that there is no activation after the
+        last layer. By design, the input and output of the block have exactly the same shape.
+        :param n_layers: int, number of intermediate layers in the residual block
+        :param input_dim: int, the dimension (or number of channels for a conv architecture) of the input to the
+        residual block.
+        :param hidden_dim: int, the dimension (or number of channels for a conv architecture) of the layers which are
+        neither the first or last layer in the block.
+        :param activation: torch.nn.Module, the activation function.
+        :param layer_type: str, either 'linear' or 'conv', representing the type of layers implemented inside the
+        residual block.
+        :param bias: bool, whether to use a bias term or not.
+        :param kwargs: dict, additional key-word arguments to be passed in the case of convolutional blocks (such as the
+        kernel size).
+        """
         super().__init__()
-        self.n_layer = n_layers
+        self.n_layers = n_layers
         self.input_dim = input_dim
         self.hidden_dim = hidden_dim
         self.activation = activation
-        self.n_layer = n_layers
+        self.n_layers = n_layers
         self.layer_type = layer_type
         self.bias = bias
 
-        self.build_layers_(**kwargs)
+        self._build_layers(**kwargs)
 
-    def build_layers_(self, **kwargs):
+    def _build_layers(self, **kwargs):
         if self.n_layers <= 1:
             raise ValueError(f"ResidualBlock only accepts at least 2 layers "
-                             f"but the number of layers was {self.n_layer}")
+                             f"but the number of layers was {self.n_layers}")
         else:
             self.layers = torch.nn.Sequential()
             if self.layer_type == "linear":
-                self.build_linear_layers_()
+                self._build_linear_layers()
 
             elif self.layer_type == "conv":
-                self.build_conv_layers_(**kwargs)
+                self._build_conv_layers(**kwargs)
             else:
                 raise ValueError(f"`layer_type` argument must be one of 'linear' or 'conv' but was {self.layer_type}")
 
-    def build_linear_layers_(self):
+    def _build_linear_layers(self):
         self.layers.add_module(name=f"linear1", module=torch.nn.Linear(in_features=self.input_dim,
                                                                        out_features=self.hidden_dim,
                                                                        bias=self.bias))
@@ -51,7 +66,7 @@ class ResidualBlock(torch.nn.Module):
                                                       out_features=self.input_dim,
                                                       bias=self.bias))
 
-    def build_conv_layers_(self, **kwargs):
+    def _build_conv_layers(self, **kwargs):
         self.layers.add_module(name=f"conv1", module=torch.nn.Conv2d(in_channels=self.input_dim,
                                                                      out_channels=self.hidden_dim,
                                                                      padding="same",
@@ -66,7 +81,7 @@ class ResidualBlock(torch.nn.Module):
             self.layers.add_module(name=f"{self.activation}{i + 1}", module=self.activation)
         self.layers.add_module(name=f"conv{self.n_layers}",
                                module=torch.nn.Conv2d(in_channels=self.hidden_dim,
-                                                      out_channels=self.hidden_dim,
+                                                      out_channels=self.input_dim,
                                                       padding="same",
                                                       bias=self.bias, **kwargs))
 

rl-solitaire/nn/policy_value/skeleton.py
@@ -1,7 +1,7 @@
 import torch
 
 from ..base_net import BaseNet
-from ..utils import get_loss, compute_entropies_from_logits
+from ..utils import compute_entropies_from_logits
 
 
 DEFAULT_ACTOR_LOSS_DICT = {"name": "cross_entropy",
@@ -145,7 +145,7 @@ class BasePolicyValueNet(BaseNet):
         else:
             regularized_loss = loss
             with torch.no_grad():
-                kl_div_uniform = kl_div_uniform = torch.nn.functional.kl_div(
+                kl_div_uniform = torch.nn.functional.kl_div(
                     torch.nn.functional.log_softmax(logits),
                     feasible_actions_uniform_dist,
                     reduction="batchmean",
@@ -165,6 +165,7 @@ class BasePolicyValueNet(BaseNet):
         # auxiliary loss metrics
         self.log('train/actor_loss', torch.mean(actor_loss).detach().item(), logger=True)
         self.log('train/advantage_actor_loss', advantage_actor_loss.detach().item(), logger=True)
+        self.log('train/advantages', torch.mean(advantages).detach().item(), logger=True)
         self.log('train/critic_loss', critic_loss.detach().item(), logger=True)
         self.log('train/weighted_actor_loss', weighted_actor_loss.detach().item(), logger=True)
         self.log('train/weighted_critic_loss', weighted_critic_loss.detach().item(), logger=True)

rl-solitaire/nn/utils.py
@@ -32,7 +32,8 @@ INIT_DICT = {'glorot_uniform': torch.nn.init.xavier_uniform_,
              'uniform': torch.nn.init.uniform_}
 DEFAULT_INIT = "he_normal"
 
-SUPPORTED_NETS_TO_CLASS_NAME = {'fc_policy_value': "policy_value.fully_connected.FCPolicyValueNet"}
+SUPPORTED_NETS_TO_CLASS_NAME = {'fc_policy_value': "policy_value.fully_connected.FCPolicyValueNet",
+                                'conv_policy_value': "policy_value.conv.ConvPolicyValueNet"}
 
 
 def get_activation(activation=None):
@@ -110,7 +111,7 @@ def compute_entropies_from_logits(logits: torch.Tensor, mask: torch.Tensor = Non
     :return:
     """
     log_probas = torch.nn.functional.log_softmax(logits, dim=1)
-    probas = torch.nn.functional.log_softmax(logits, dim=1)
+    probas = torch.nn.functional.softmax(logits, dim=1)
     p_log_p = probas * log_probas
     if mask is None:
         return torch.sum(p_log_p, dim=1), torch.Tensor([])