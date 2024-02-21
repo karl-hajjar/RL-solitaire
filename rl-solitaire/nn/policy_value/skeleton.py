Fixed issue with entropy computation. Tested conv output sizes.

rl-solitaire/nn/policy_value/skeleton.py
@@ -129,7 +129,12 @@ class BasePolicyValueNet(BaseNet):
             if self.regularization_type == "entropy":
                 kl_div_uniform = self.regularization_loss(torch.nn.functional.log_softmax(logits, dim=-1),
                                                           feasible_actions_uniform_dist)
-                regularized_loss = loss + self.regularization_coef * kl_div_uniform
+                weighted_penalty = self.regularization_coef * kl_div_uniform
+                regularized_loss = loss + weighted_penalty
+                self.log('train/penalty', kl_div_uniform.detach().item(),
+                         on_step=True, logger=True)
+                self.log('train/weighted_penalty', weighted_penalty.detach().item(),
+                         prog_bar=True, on_step=True, logger=True)
                 self.log('train/regularized_loss', regularized_loss.detach().item(),
                          prog_bar=True, on_step=True, logger=True)
             else:

rl-solitaire/nn/utils.py
@@ -114,9 +114,9 @@ def compute_entropies_from_logits(logits: torch.Tensor, mask: torch.Tensor = Non
     probas = torch.nn.functional.softmax(logits, dim=1)
     p_log_p = probas * log_probas
     if mask is None:
-        return torch.sum(p_log_p, dim=1), torch.Tensor([])
+        return -torch.sum(p_log_p, dim=1), torch.Tensor([])
     else:
-        return torch.sum(p_log_p, dim=1), torch.sum(mask * p_log_p, dim=1)
+        return -torch.sum(p_log_p, dim=1), -torch.sum(mask * p_log_p, dim=1)
 
 
 def get_network_class_from_name(name: str):