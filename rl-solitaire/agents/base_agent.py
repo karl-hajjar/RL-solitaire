Added PPO agent/ Fixed issue with agent tests with integer dtypes. Fixed issue with get_policy method of networks which was missing the softmax

rl-solitaire/agents/base_agent.py
@@ -79,7 +79,7 @@ class BaseAgent:
         return action_index
 
     def collect_data(self, env, T=None) -> dict[str, np.ndarray]:
-        states, actions, action_masks, rewards, next_state, end = self.collect_data_(env, T=None)
+        states, actions, action_masks, rewards, next_state, end = self.collect_data_(env, T=T)
         return self._format_data(states, actions, action_masks, rewards, next_state, end)
 
     def collect_data_(self, env, T=None) -> (list[np.ndarray], list[int], list[float], list[float], np.ndarray, bool):

rl-solitaire/agents/ppo/ppo_agent.py
@@ -4,8 +4,8 @@ from ..base_agent import BaseAgent
 import torch
 
 
-class ActorCriticAgent(BaseAgent):
-    """ActorCriticAgent implements a class of agents using the actor-critic method."""
+class PPOAgent(BaseAgent):
+    """PPOAgent implements a class of agents using Proximal Policy Optimization (PPO) methods."""
 
     def __init__(self, network: torch.nn.Module, name="ActorCriticAgent", discount=1.0):
         super().__init__(name, discount)
@@ -42,6 +42,10 @@ class ActorCriticAgent(BaseAgent):
         states = np.array(states)
         bootstrapped_state_values = self.get_value(states).reshape(-1)
 
+        # get old policy for each all states in a batch to save time
+        old_policies = self.get_policy(states)
+        actions = np.array(actions)
+
         assert (t == len(rewards) == len(actions) == len(bootstrapped_state_values))
         reversed_value_targets = []
         reversed_advantages = []
@@ -51,7 +55,8 @@ class ActorCriticAgent(BaseAgent):
             reversed_advantages.append(value - bootstrapped_state_values[t - s - 1])
 
         return {"states": states,
-                "actions": np.array(actions),
+                "actions": actions,
+                "action_probas": old_policies[np.arange(t), actions].reshape(-1, 1),
                 "action_masks": np.array(action_masks).astype(np.float32),
                 "advantages": np.array(reversed_advantages[::-1]).reshape(-1, 1).astype(np.float32),
                 "value_targets": np.array(reversed_value_targets[::-1]).reshape(-1, 1).astype(np.float32)}

rl-solitaire/agents/ppo/ppo_trainer.py
@@ -1,5 +1,6 @@
 import logging
 import os
+
 import numpy as np
 import torch
 from pytorch_lightning import Trainer
@@ -7,46 +8,48 @@ from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning.loggers import TensorBoardLogger, NeptuneLogger
 from torch.utils.data import Dataset, DataLoader
 
-from agents.actor_critic.actor_critic_agent import ActorCriticAgent
+from agents.ppo.ppo_agent import PPOAgent
 from agents.trainer import BaseTrainer, NEPTUNE_PROJECT_NAME
 from env.env import Env
 
 logger = logging.getLogger()
 
-
 SUPPORTED_LOGGER_NAMES = {"tensorboard", "neptune", "wandb"}
 
 
-class ActorCriticDataset(Dataset):
-    def __init__(self, states: torch.Tensor, action_indices: torch.Tensor, action_masks: torch.Tensor,
-                 advantages: torch.Tensor, value_targets: torch.Tensor):
+class PPODataset(Dataset):
+    def __init__(self, states: torch.Tensor, action_indices: torch.Tensor, action_probas: torch.Tensor,
+                 action_masks: torch.Tensor, advantages: torch.Tensor, value_targets: torch.Tensor):
         super().__init__()
         assert (len(states) == len(action_indices) == len(action_masks) == len(advantages) == len(value_targets))
         self.states = states
         self.action_indices = action_indices.long()
+        self.action_probas = action_probas
         self.action_masks = action_masks
         self.advantages = advantages
         self.value_targets = value_targets
 
     def __getitem__(self, index) -> (torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor):
-        return self.states[index], self.action_indices[index], self.action_masks[index], self.advantages[index], \
+        return self.states[index], self.action_indices[index], self.action_probas[index], self.action_masks[index], \
+               self.advantages[index], \
                self.value_targets[index]
 
     def __len__(self):
         return len(self.states)
 
 
-class ActorCriticTrainer(BaseTrainer):
+class PPOTrainer(BaseTrainer):
     """
-    A Trainer class for actor-critic agents.
+    A Trainer class for PPO agents.
     """
-    def __init__(self, env: Env, agent: ActorCriticAgent, n_iter: int, n_games_train: int, agent_results_filepath: str,
+
+    def __init__(self, env: Env, agent: PPOAgent, n_iter: int, n_games_train: int, agent_results_filepath: str,
                  n_steps_update: int = None, log_every: int = None, n_games_eval: int = 10, n_optim_steps: int = None,
                  batch_size: int = 64, log_dir: str = None, checkpoints_dir: str = None):
         super().__init__(env, agent, n_iter, n_games_train, agent_results_filepath, n_steps_update, log_every,
                          n_games_eval)
         # TODO: create a config class for trainers
-        self._name = "ActorCriticTrainer"
+        self._name = "PPOTrainer"
         self.batch_size = batch_size
         self.checkpoints_dir = checkpoints_dir
 
@@ -94,15 +97,16 @@ class ActorCriticTrainer(BaseTrainer):
             raise ValueError(f"Logger with name {name} is not supported. Supported logger names are : "
                              f"{SUPPORTED_LOGGER_NAMES}")
 
-    def prepare_dataset(self, data: dict[str, np.array]) -> ActorCriticDataset:
+    def prepare_dataset(self, data: dict[str, np.array]) -> PPODataset:
         data = self.reformat_data(data)
-        return ActorCriticDataset(states=data["states"],
-                                  action_indices=data["actions"],
-                                  action_masks=data["action_masks"],
-                                  advantages=data["advantages"],
-                                  value_targets=data["value_targets"])
+        return PPODataset(states=data["states"],
+                          action_indices=data["actions"],
+                          action_probas=data["action_probas"],
+                          action_masks=data["action_masks"],
+                          advantages=data["advantages"],
+                          value_targets=data["value_targets"])
 
-    def prepare_dataloader(self, dataset: ActorCriticDataset) -> DataLoader:
+    def prepare_dataloader(self, dataset: PPODataset) -> DataLoader:
         return DataLoader(dataset, shuffle=True, batch_size=self.batch_size)
 
     def reformat_data(self, data: dict[str, np.array]) -> dict[str, torch.Tensor]:

rl-solitaire/agents/utils.py
@@ -1,7 +1,7 @@
 import numpy as np
 import importlib
 
-SUPPORTED_AGENTS = {'actor_critic'}
+SUPPORTED_AGENTS = {'actor_critic', 'ppo'}
 
 
 def mask_infeasible_actions(policy: np.array, feasible_action_indices: np.array, normalize=True) -> np.array:
@@ -42,7 +42,10 @@ def get_class_name_from_name(name: str, class_type) -> str:
         raise ValueError(f"`class_type` argument must be 'Agent' or 'Trainer' but was {class_type}")
     try:
         names = name.split('_')
-        class_name = ''.join([name_[0].upper() + name_[1:] for name_ in names]) + class_type
+        if len(names) == 1:
+            class_name = names[0].upper() + class_type
+        else:
+            class_name = ''.join([name_[0].upper() + name_[1:] for name_ in names]) + class_type
     except:
         try:
             names = name.split('_')

rl-solitaire/nn/policy_value/conv.py
@@ -1,4 +1,5 @@
 import torch
+from torch.nn.functional import softmax
 
 from nn.blocks.residual import ResidualBlock
 from nn.network_config import NetConfig
@@ -95,7 +96,7 @@ class ConvPolicyValueNet(BasePolicyValueNet):
         return x.reshape(x.shape[0], x.shape[-1], x.shape[1], x.shape[2])
 
     def get_policy(self, x: torch.Tensor) -> torch.Tensor:
-        return self.policy_head(self.state_embeddings(self._reshape_2d_input(x)))
+        return softmax(self.policy_head(self.state_embeddings(self._reshape_2d_input(x))), dim=-1)
 
     def get_value(self, x: torch.Tensor) -> torch.Tensor:
         return self.value_head(self.state_embeddings(self._reshape_2d_input(x)))

rl-solitaire/nn/policy_value/skeleton.py
@@ -1,4 +1,5 @@
 import torch
+from torch.nn.functional import softmax, log_softmax, kl_div
 
 from ..base_net import BaseNet
 from ..utils import compute_entropies_from_logits
@@ -67,7 +68,7 @@ class BasePolicyValueNet(BaseNet):
         self.critic_loss = self._get_loss(loss_config["critic_loss"])
 
     def get_policy(self, x: torch.Tensor):
-        return self.policy_head(self.state_embeddings(x))
+        return softmax(self.policy_head(self.state_embeddings(x)), dim=-1)
 
     def get_value(self, x: torch.Tensor):
         return self.value_head(self.state_embeddings(x))
@@ -111,11 +112,18 @@ class BasePolicyValueNet(BaseNet):
         :param batch_nb: int, the index of the current batch
         :return: dict with the training metrics.
         """
-        states, action_indices, action_masks, advantages, value_targets = batch
-        logits, values = self.forward(states)
+        if len(batch) == 5:  # standard way of training the network
+            states, action_indices, action_masks, advantages, value_targets = batch
+            logits, values = self.forward(states)
+            actor_loss = self.actor_loss(logits, action_indices)
+            advantage_actor_loss = torch.mean(advantages * actor_loss)
+        elif len(batch) == 6:  # PPO network needs action probabilities from old policy
+            states, action_indices, action_probas, action_masks, advantages, value_targets = batch
+            logits, values = self.forward(states)
+            advantage_actor_loss = self.actor_loss(logits, action_indices, action_probas, advantages)
+        else:
+            raise ValueError(f"Batch length must be 5 or 6 but was {len(batch)}")
 
-        actor_loss = self.actor_loss(logits, action_indices)
-        advantage_actor_loss = torch.mean(advantages * actor_loss)
         critic_loss = self.critic_loss(values, value_targets)
         weighted_actor_loss = self.actor_coef * advantage_actor_loss
         weighted_critic_loss = self.critic_coef * critic_loss
@@ -164,13 +172,17 @@ class BasePolicyValueNet(BaseNet):
             self.log('train/full_entropy', torch.mean(full_entropy).detach().item(), logger=True)
             self.log('train/masked_entropy', torch.mean(masked_entropy).detach().item(), logger=True)
             self.log('train/kl_div_uniform', kl_div_uniform.detach().item(), on_step=True, logger=True)
+            self.log('train/feasible_actions_proba', 
+                     torch.mean(torch.sum(softmax(logits) * action_masks, dim=-1)).detach().item(),
+                     on_step=True, logger=True)
             self.log('train/n_feasible_actions', torch.mean(torch.sum(action_masks, dim=-1)).detach().item(),
                      on_step=True, logger=True)
 
         # auxiliary loss metrics
-        self.log('train/actor_loss', torch.mean(actor_loss).detach().item(), logger=True)
-        self.log('train/advantage_actor_loss', advantage_actor_loss.detach().item(), logger=True)
+        if len(batch) == 5:
+            self.log('train/actor_loss', torch.mean(actor_loss).detach().item(), logger=True)
         self.log('train/advantages', torch.mean(advantages).detach().item(), logger=True)
+        self.log('train/advantage_actor_loss', advantage_actor_loss.detach().item(), logger=True)
         self.log('train/critic_loss', critic_loss.detach().item(), logger=True)
         self.log('train/weighted_actor_loss', weighted_actor_loss.detach().item(), logger=True)
         self.log('train/weighted_critic_loss', weighted_critic_loss.detach().item(), logger=True)

rl-solitaire/nn/utils.py
@@ -16,14 +16,6 @@ OPTIMIZER_DICT = {'adam': torch.optim.Adam,
                   'sgd': torch.optim.SGD}
 DEFAULT_OPT = "adam"
 
-LOSS_DICT = {'cross_entropy': torch.nn.CrossEntropyLoss,
-             'bce'
-             'kl': torch.nn.KLDivLoss,
-             'mse': torch.nn.MSELoss,
-             'logistic': torch.nn.BCEWithLogitsLoss}
-
-DEFAULT_LOSS = "cross_entropy"
-
 INIT_DICT = {'glorot_uniform': torch.nn.init.xavier_uniform_,
              'glorot_normal': torch.nn.init.xavier_normal_,
              'he_uniform': torch.nn.init.kaiming_uniform_,
@@ -34,6 +26,62 @@ DEFAULT_INIT = "he_normal"
 
 SUPPORTED_NETS_TO_CLASS_NAME = {'fc_policy_value': "policy_value.fully_connected.FCPolicyValueNet",
                                 'conv_policy_value': "policy_value.conv.ConvPolicyValueNet"}
+
+
+class PPOClipLoss(torch.nn.Module):
+    SUPPORTED_REDUCTIONS = {"none", "mean", "sum"}
+
+    def __init__(self, reduction: str = 'mean', epsilon_clip: float = 0.2, epsilon_proba_ratio: float = 1e-6) -> None:
+        """
+        A class implementing the PPO clipped loss. First, we compute the ratio of new over old action probabilities as
+        well as a clipped version between 1-epsilon_clip and 1+epsilon_clip. Then we multiply both by the advantages and
+        return the minimum of the two values.
+        :param reduction: string indicating which type of reduction to apply to the batch losses. Must be one of
+        {"none", "mean", "sum"}.
+        :param epsilon_clip: float indicating the value to use when clipping around 1.
+        :param epsilon_proba_ratio: float indicating the value to add to the old action probas to prevent numerical
+        overflow when the probabilities are too low.
+        """
+        super().__init__()
+        if reduction not in self.SUPPORTED_REDUCTIONS:
+            raise ValueError(f"`reduction` argument must be one of {self.SUPPORTED_REDUCTIONS}, but was {reduction}")
+        self.reduction = reduction
+        self.epsilon_clip = epsilon_clip
+        self.epsilon_proba_ratio = epsilon_proba_ratio
+
+    def forward(self, logits: torch.Tensor, action_indices: torch.Tensor, action_probas: torch.Tensor,
+                advantages: torch.Tensor) -> torch.Tensor:
+        """
+        Computes the PPO clipped loss.
+        :param logits: Tensor of shape (N, N_ACTIONS) containing the logits of the possible actions for each state in the batch.
+        :param action_indices: Tensor of shape (N, 1) containing the index of the actions taken in the batch.
+        :param action_probas: Tensor of shape (N, 1) containing the old policy probabilities for the actions taken.
+        :param advantages: Tensor of shape (N, 1) containing the advantage estimated for each state-action in the batch.
+        :return: Tensor of shape (1) if reduction is "mean" or "sum", and of shape (N, 1) if reduction is "none".
+        """
+        probas = torch.nn.functional.softmax(logits, dim=-1)  # compute softmax over last dimension
+        new_action_probas = probas[torch.arange(len(action_indices)), action_indices]
+        proba_ratios = new_action_probas / (action_probas + self.epsilon_proba_ratio)
+        clipped_proba_ratios = torch.clip(proba_ratios, min=1-self.epsilon_clip, max=1 + self.epsilon_clip)
+
+        # maximizing advantage * action_proba <=> minimizing - loss
+        batch_losses = -torch.minimum(proba_ratios * advantages, clipped_proba_ratios * advantages)
+        if self.reduction == "none":
+            return batch_losses
+        elif self.reduction == "mean":
+            return torch.mean(batch_losses)
+        else:
+            return torch.sum(batch_losses)
+
+
+LOSS_DICT = {'cross_entropy': torch.nn.CrossEntropyLoss,
+             'ppo_clip': PPOClipLoss,
+             'bce': torch.nn.BCELoss,
+             'kl': torch.nn.KLDivLoss,
+             'mse': torch.nn.MSELoss,
+             'logistic': torch.nn.BCEWithLogitsLoss}
+
+DEFAULT_LOSS = "cross_entropy"
 
 
 def get_activation(activation=None):

rl-solitaire/tests/agents/ppo/test_ppo_agent.py
@@ -6,52 +6,63 @@ import pickle
 
 from nn.network_config import NetConfig
 from nn.policy_value.fully_connected import FCPolicyValueNet
-from agents.actor_critic.actor_critic_agent import ActorCriticAgent
-from agents.actor_critic.actor_critic_trainer import ActorCriticTrainer
+from agents.ppo.ppo_agent import PPOAgent
+from agents.ppo.ppo_trainer import PPOTrainer
 from env.env import Env, N_STATE_CHANNELS, N_ACTIONS
+from agents.utils import get_class_from_name
 
 ROOT = os.path.dirname(os.path.dirname(os.path.dirname(os.getcwd())))
 FC_POLICY_VALUE_CONFIG_FILE_PATH = os.path.join(ROOT, "nn/policy_value/fc_policy_value_config.yaml")
-RESOURCES_DIR = os.path.join(ROOT, "tests/agents/actor_critic/resources")
+RESOURCES_DIR = os.path.join(ROOT, "tests/agents/ppo/resources")
 
 
-class TestActorCriticTrainer(unittest.TestCase):
+class TestPPOAgent(unittest.TestCase):
     def setUp(self) -> None:
         self.net_config = NetConfig(config_path=FC_POLICY_VALUE_CONFIG_FILE_PATH)
         self.log_dir = RESOURCES_DIR
         self.checkpoints_dir = os.path.join(RESOURCES_DIR, "checkpoints")
         self.agent_results_filepath = os.path.join(RESOURCES_DIR, "results.pickle")
 
-    def test_trainer_init(self):
-        network = FCPolicyValueNet(config=self.net_config)
-        agent = ActorCriticAgent(network=network, name="FCActorCriticAgent")
-        env = Env()
-        trainer = ActorCriticTrainer(env, agent, n_iter=2, n_games_train=5,
-                                     agent_results_filepath=self.agent_results_filepath, log_every=1,
-                                     log_dir=self.log_dir, checkpoints_dir=self.checkpoints_dir)
-
-        self.assertEqual(trainer.trainer.max_epochs, 0)
-        self.assertEqual(trainer.trainer.max_steps, -1)
-
-        n_optim_steps = 50
-        trainer = ActorCriticTrainer(env, agent, n_iter=2, n_games_train=5, n_optim_steps=n_optim_steps,
-                                     agent_results_filepath=self.agent_results_filepath, log_every=1,
-                                     log_dir=self.log_dir, checkpoints_dir=self.checkpoints_dir)
-
-        self.assertIsNone(trainer.trainer.max_epochs)
-        self.assertEqual(trainer.trainer.max_steps, 0)
-
     def test_collect_data(self):
         network = FCPolicyValueNet(config=self.net_config)
-        agent = ActorCriticAgent(network=network, name="FCActorCriticAgent")
+        agent = PPOAgent(network=network, name="PPOAgent")
         env = Env()
-        trainer = ActorCriticTrainer(env, agent, n_iter=2, n_games_train=5,
+        T = 10
+        with torch.no_grad():
+            data = agent.collect_data(env, T=T)
+        for key in ["states", "actions", "action_probas", "action_masks", "advantages", "value_targets"]:
+            self.assertIn(key, data.keys())
+            self.assertTrue(type(data[key]) == np.ndarray)
+
+        T = len(data["states"])
+        self.assertSequenceEqual(data["states"].shape, (T, 7, 7, N_STATE_CHANNELS))
+        self.assertSequenceEqual(data["actions"].shape, (T,))
+        self.assertSequenceEqual(data["action_probas"].shape, (T, 1))
+        self.assertSequenceEqual(data["action_masks"].shape, (T, N_ACTIONS))
+        self.assertSequenceEqual(data["advantages"].shape, (T, 1))
+        self.assertSequenceEqual(data["value_targets"].shape, (T, 1))
+
+        self.assertTrue(data["states"].dtype == np.float32)
+        self.assertTrue(data["actions"].dtype == int)
+        self.assertTrue(data["action_probas"].dtype == np.float32)
+        self.assertTrue(data["action_masks"].dtype == np.float32)
+        self.assertTrue(data["advantages"].dtype == np.float32)
+        self.assertTrue(data["value_targets"].dtype == np.float32)
+
+        self.assertTrue((data["action_probas"] >= 0).all())
+        self.assertTrue((data["action_probas"] <= 1).all())
+
+    def test_trainer_collect_data(self):
+        network = FCPolicyValueNet(config=self.net_config)
+        agent = PPOAgent(network=network, name="PPOAgent")
+        env = Env()
+        trainer = PPOTrainer(env, agent, n_iter=2, n_games_train=5,
                                      agent_results_filepath=self.agent_results_filepath, log_every=1,
                                      log_dir=self.log_dir, checkpoints_dir=self.checkpoints_dir)
         with torch.no_grad():
             data = trainer.collect_data()
 
-        for key in ["states", "actions", "action_masks", "advantages", "value_targets"]:
+        for key in ["states", "actions", "action_masks", "action_probas", "advantages", "value_targets"]:
             self.assertIn(key, data.keys())
             self.assertTrue(type(data[key]) == np.ndarray)
 
@@ -59,21 +70,23 @@ class TestActorCriticTrainer(unittest.TestCase):
         self.assertGreater(T, trainer.n_games_train)
         self.assertSequenceEqual(data["states"].shape, (T, 7, 7, N_STATE_CHANNELS))
         self.assertSequenceEqual(data["actions"].shape, (T,))
+        self.assertSequenceEqual(data["action_probas"].shape, (T, 1))
         self.assertSequenceEqual(data["action_masks"].shape, (T, N_ACTIONS))
         self.assertSequenceEqual(data["advantages"].shape, (T, 1))
         self.assertSequenceEqual(data["value_targets"].shape, (T, 1))
 
         self.assertTrue(data["states"].dtype == np.float32)
         self.assertTrue(data["actions"].dtype == int)
+        self.assertTrue(data["action_probas"].dtype == np.float32)
         self.assertTrue(data["action_masks"].dtype == np.float32)
         self.assertTrue(data["advantages"].dtype == np.float32)
         self.assertTrue(data["value_targets"].dtype == np.float32)
 
     def test_reformat_data(self):
         network = FCPolicyValueNet(config=self.net_config)
-        agent = ActorCriticAgent(network=network, name="FCActorCriticAgent")
+        agent = PPOAgent(network=network, name="PPOAgent")
         env = Env()
-        trainer = ActorCriticTrainer(env, agent, n_iter=2, n_games_train=5,
+        trainer = PPOTrainer(env, agent, n_iter=2, n_games_train=5,
                                      agent_results_filepath=self.agent_results_filepath, log_every=1,
                                      log_dir=self.log_dir, checkpoints_dir=self.checkpoints_dir)
 
@@ -82,7 +95,7 @@ class TestActorCriticTrainer(unittest.TestCase):
             T_ = len(data["states"])
             data = trainer.reformat_data(data)
 
-            for key in ["states", "actions", "action_masks", "advantages", "value_targets"]:
+            for key in ["states", "actions", "action_probas", "action_masks", "advantages", "value_targets"]:
                 self.assertIn(key, data.keys())
                 self.assertTrue(type(data[key]) == torch.Tensor)
 
@@ -91,21 +104,23 @@ class TestActorCriticTrainer(unittest.TestCase):
             self.assertGreater(T, trainer.n_games_train)
             self.assertSequenceEqual(data["states"].shape, (T, 7, 7, N_STATE_CHANNELS))
             self.assertSequenceEqual(data["actions"].shape, (T,))
+            self.assertSequenceEqual(data["action_probas"].shape, (T, 1))
             self.assertSequenceEqual(data["action_masks"].shape, (T, N_ACTIONS))
             self.assertSequenceEqual(data["advantages"].shape, (T, 1))
             self.assertSequenceEqual(data["value_targets"].shape, (T, 1))
 
             self.assertTrue(data["states"].dtype == torch.float32)
             self.assertTrue(data["actions"].dtype == torch.int64)
+            self.assertTrue(data["action_probas"].dtype == torch.float32)
             self.assertTrue(data["action_masks"].dtype == torch.float32)
             self.assertTrue(data["advantages"].dtype == torch.float32)
             self.assertTrue(data["value_targets"].dtype == torch.float32)
 
     def test_prepare_dataset(self):
         network = FCPolicyValueNet(config=self.net_config)
-        agent = ActorCriticAgent(network=network, name="FCActorCriticAgent")
+        agent = PPOAgent(network=network, name="PPOAgent")
         env = Env()
-        trainer = ActorCriticTrainer(env, agent, n_iter=2, n_games_train=5,
+        trainer = PPOTrainer(env, agent, n_iter=2, n_games_train=5,
                                      agent_results_filepath=self.agent_results_filepath, log_every=1,
                                      log_dir=self.log_dir, checkpoints_dir=self.checkpoints_dir)
 
@@ -114,7 +129,7 @@ class TestActorCriticTrainer(unittest.TestCase):
             T_ = len(data["states"])
             dataset = trainer.prepare_dataset(data)
 
-            for key in ["states", "action_indices", "action_masks", "advantages", "value_targets"]:
+            for key in ["states", "action_indices", "action_probas", "action_masks", "advantages", "value_targets"]:
                 self.assertTrue(hasattr(dataset, key))
                 self.assertTrue(type(getattr(dataset, key)) == torch.Tensor)
 
@@ -123,12 +138,14 @@ class TestActorCriticTrainer(unittest.TestCase):
             self.assertGreater(T, trainer.n_games_train)
             self.assertSequenceEqual(dataset.states.shape, (T, 7, 7, N_STATE_CHANNELS))
             self.assertSequenceEqual(dataset.action_indices.shape, (T,))
+            self.assertSequenceEqual(dataset.action_probas.shape, (T, 1))
             self.assertSequenceEqual(dataset.action_masks.shape, (T, N_ACTIONS))
             self.assertSequenceEqual(dataset.advantages.shape, (T, 1))
             self.assertSequenceEqual(dataset.value_targets.shape, (T, 1))
 
             self.assertTrue(dataset.states.dtype == torch.float32)
             self.assertTrue(dataset.action_indices.dtype == torch.int64)
+            self.assertTrue(dataset.action_probas.dtype == torch.float32)
             self.assertTrue(dataset.action_masks.dtype == torch.float32)
             self.assertTrue(dataset.advantages.dtype == torch.float32)
             self.assertTrue(dataset.value_targets.dtype == torch.float32)
@@ -137,16 +154,20 @@ class TestActorCriticTrainer(unittest.TestCase):
             item = dataset[i]
             self.assertSequenceEqual(item[0].shape, (7, 7, N_STATE_CHANNELS))
             self.assertSequenceEqual(item[1].shape, ())
-            self.assertSequenceEqual(item[2].shape, (N_ACTIONS,))
-            self.assertSequenceEqual(item[3].shape, (1,))
+            self.assertSequenceEqual(item[2].shape, (1,))  # action_probas attribute
+            self.assertSequenceEqual(item[3].shape, (N_ACTIONS,))
             self.assertSequenceEqual(item[4].shape, (1,))
+            self.assertSequenceEqual(item[5].shape, (1,))
+
+            self.assertTrue((dataset.action_probas >= 0).all())
+            self.assertTrue((dataset.action_probas <= 1).all())
 
     def test_prepare_dataloader(self):
         network = FCPolicyValueNet(config=self.net_config)
-        agent = ActorCriticAgent(network=network, name="FCActorCriticAgent")
+        agent = PPOAgent(network=network, name="PPOAgent")
         env = Env()
         batch_size = 16
-        trainer = ActorCriticTrainer(env, agent, n_iter=2, n_games_train=5, batch_size=batch_size,
+        trainer = PPOTrainer(env, agent, n_iter=2, n_games_train=5, batch_size=batch_size,
                                      agent_results_filepath=self.agent_results_filepath, log_every=1,
                                      log_dir=self.log_dir, checkpoints_dir=self.checkpoints_dir)
 
@@ -161,10 +182,11 @@ class TestActorCriticTrainer(unittest.TestCase):
             N, r = divmod(T, batch_size)
             for i, batch in enumerate(dataloader):
                 # self.states, self.action_indices, self.action_masks, self.advantages, self.value_targets
-                states, action_indices, action_masks, advantages, value_targets = batch
+                states, action_indices, action_probas, action_masks, advantages, value_targets = batch
 
                 self.assertTrue(type(states) == torch.Tensor)
                 self.assertTrue(type(action_indices) == torch.Tensor)
+                self.assertTrue(type(action_probas) == torch.Tensor)
                 self.assertTrue(type(action_masks) == torch.Tensor)
                 self.assertTrue(type(advantages) == torch.Tensor)
                 self.assertTrue(type(value_targets) == torch.Tensor)
@@ -172,30 +194,74 @@ class TestActorCriticTrainer(unittest.TestCase):
                 if i < N:
                     self.assertSequenceEqual(states.shape, (batch_size, 7, 7, N_STATE_CHANNELS))
                     self.assertSequenceEqual(action_indices.shape, (batch_size,))
+                    self.assertSequenceEqual(action_probas.shape, (batch_size, 1))
                     self.assertSequenceEqual(action_masks.shape, (batch_size, N_ACTIONS))
                     self.assertSequenceEqual(advantages.shape, (batch_size, 1))
                     self.assertSequenceEqual(value_targets.shape, (batch_size, 1))
                 else:
                     self.assertSequenceEqual(states.shape, (r, 7, 7, N_STATE_CHANNELS))
                     self.assertSequenceEqual(action_indices.shape, (r,))
+                    self.assertSequenceEqual(action_probas.shape, (r, 1))
                     self.assertSequenceEqual(action_masks.shape, (r, N_ACTIONS))
                     self.assertSequenceEqual(advantages.shape, (r, 1))
                     self.assertSequenceEqual(value_targets.shape, (r, 1))
 
                 self.assertTrue(states.dtype == torch.float32)
                 self.assertTrue(action_indices.dtype == torch.int64)
+                self.assertTrue(action_probas.dtype == torch.float32)
                 self.assertTrue(action_masks.dtype == torch.float32)
                 self.assertTrue(advantages.dtype == torch.float32)
                 self.assertTrue(value_targets.dtype == torch.float32)
 
+    def test_agent_loss(self):
+        net_config = NetConfig(config_path=FC_POLICY_VALUE_CONFIG_FILE_PATH)
+
+        reduction = "mean"
+        epsilon_clip = 0.3
+        net_config.loss_config["actor_loss"] = {"name": "ppo_clip",
+                                                "coef": 1.0,
+                                                "reduction": reduction,
+                                                "epsilon_clip": epsilon_clip}
+
+        network = FCPolicyValueNet(config=net_config)
+        agent = PPOAgent(network=network, name="PPOAgent")
+
+        # test loss attributes
+        self.assertTrue(hasattr(agent.network.actor_loss, "reduction"))
+        self.assertTrue(hasattr(agent.network.actor_loss, "epsilon_clip"))
+        self.assertTrue(hasattr(agent.network.actor_loss, "epsilon_proba_ratio"))
+
+        # test attributes values
+        self.assertEqual(agent.network.actor_loss.reduction, reduction)
+        self.assertEqual(agent.network.actor_loss.epsilon_clip, epsilon_clip)
+
+        batch_size = 8
+        logits = torch.randn(batch_size, N_ACTIONS)
+        action_indices = torch.randint(low=0, high=N_ACTIONS, size=(batch_size, ))
+        all_action_probas = torch.nn.functional.softmax(torch.randn(batch_size, N_ACTIONS), dim=-1)
+        action_probas = all_action_probas[torch.arange(batch_size), action_indices]
+        advantages = torch.randn(batch_size, 1)
+
+        loss = agent.network.actor_loss(logits, action_indices, action_probas, advantages)
+        self.assertSequenceEqual(loss.shape, ())
+
     def test_train(self):
-        network = FCPolicyValueNet(config=self.net_config)
-        agent = ActorCriticAgent(network=network, name="FCActorCriticAgent")
+        net_config = NetConfig(config_path=FC_POLICY_VALUE_CONFIG_FILE_PATH)
+
+        reduction = "mean"
+        epsilon_clip = 0.3
+        net_config.loss_config["actor_loss"] = {"name": "ppo_clip",
+                                                "coef": 1.0,
+                                                "reduction": reduction,
+                                                "epsilon_clip": epsilon_clip}
+
+        network = FCPolicyValueNet(config=net_config)
+        agent = PPOAgent(network=network, name="PPOAgent")
         env = Env()
         n_iter = 2
         n_games_train = 20
         n_games_eval = 50
-        trainer = ActorCriticTrainer(env, agent, n_iter=n_iter, n_games_train=n_games_train, n_games_eval=n_games_eval,
+        trainer = PPOTrainer(env, agent, n_iter=n_iter, n_games_train=n_games_train, n_games_eval=n_games_eval,
                                      agent_results_filepath=self.agent_results_filepath, log_every=1,
                                      log_dir=self.log_dir, checkpoints_dir=self.checkpoints_dir)
 
@@ -215,6 +281,13 @@ class TestActorCriticTrainer(unittest.TestCase):
             self.assertTrue(type(results[i]["pegs_left"]) == list)
             self.assertEqual(len(results[i]["rewards"]), trainer.n_games_eval)
             self.assertEqual(len(results[i]["pegs_left"]), trainer.n_games_eval)
+
+    def test_get_agent_and_trainer_class_from_names(self):
+        agent_name = 'ppo'
+        agent_class = get_class_from_name(name=agent_name, class_type="agent")
+        trainer_class = get_class_from_name(name=agent_name, class_type="trainer")
+        self.assertEqual(agent_class.__name__, "PPOAgent")
+        self.assertEqual(trainer_class.__name__, "PPOTrainer")
 
 
 if __name__ == '__main__':